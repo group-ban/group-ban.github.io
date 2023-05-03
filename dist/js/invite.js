4b1d46c60d84d43ff21a850d8e603cb977060b69
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-03 18:13:12 +0430

    Improve UI & Text Quality

--- dist/js/invite.js
+++ dist/js/invite.js
@@ -1,11 +1,5 @@
-const inviteToggle = document.querySelector(".switch");
-inviteToggle.addEventListener("change", function(){
-    if (!document.querySelector(".switch").children[0].disabled){
-        document.querySelector(".switch").children[0].disabled = true;
-        setInterval(function(){
-            document.querySelector(".switch").style.display = "none";
-            document.querySelector(".question-box").children[0].style.display = "inline-block"; 
-            document.querySelector(".question-box").children[1].innerText = "از انتخابت متشکریم"
-        }, 220)
-    }
-})
+function onSelectSurvey(isGood){
+    document.getElementById("qu-buttons").style.display = "none";
+    document.querySelector(".question-box").children[(isGood) ? 0 : 1].style.display = "inline-block"; 
+    document.getElementById("qu-text").innerText = (isGood) ? "از انتخابت متشکریم" : "از تجربه ات متاستفیم؛ برای بهبودی مشکل با پشتیبانی ربات تماس بگیرید";
+}
